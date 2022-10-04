402b3a689b50952c6befd66ea90d0aedc240f8f6
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-04 17:54:00 +0000

    Adding numeric_owner as keyword arguement

--- Data/cub2011.py
+++ Data/cub2011.py
@@ -80,7 +80,7 @@ class Cub2011(Dataset):
                     if not is_within_directory(path, member_path):
                         raise Exception("Attempted Path Traversal in Tar File")
             
-                tar.extractall(path, members, numeric_owner) 
+                tar.extractall(path, members, numeric_owner=numeric_owner) 
                 
             
             safe_extract(tar, path=self.root)
